254e5e699d44e321c7b22b615937fcef37031ad3
Moshe Immerman <moshe.immerman@gmail.com>
2019-10-25 13:34:27 +0200

    add junit test output structs

--- console/test_results.go
+++ console/test_results.go
@@ -9,8 +9,21 @@ type TestResults struct {
 	PassCount int
 	FailCount int
 	SkipCount int
+	Tests     []JUnitTestCase
 }
 
+func (c TestResults) ToXML() (string, error) {
+	return JUnitTestSuites{
+		Suites: []JUnitTestSuite{
+			JUnitTestSuite{
+				TestCases: c.Tests,
+				Tests:     c.PassCount + c.FailCount + c.SkipCount,
+				Failures:  c.FailCount,
+			},
+		},
+	}.ToXML()
+
+}
 func (c TestResults) String() string {
 	return fmt.Sprintf("  %d passed, %d skipped, %d failed\n", c.PassCount, c.SkipCount, c.FailCount)
 }
@@ -22,19 +35,36 @@ func (c *TestResults) Done() {
 
 // Passf reports a new passing test
 func (c *TestResults) Passf(name, msg string, args ...interface{}) {
+	c.Tests = append(c.Tests, JUnitTestCase{
+		Classname: name,
+		Name:      fmt.Sprintf(msg, args...),
+	})
 	c.PassCount++
 	fmt.Println(Greenf(" [pass] "+msg, args...))
-
 }
 
 // Failf reports a new failing test
 func (c *TestResults) Failf(name, msg string, args ...interface{}) {
+	c.Tests = append(c.Tests, JUnitTestCase{
+		Classname: name,
+		Name:      fmt.Sprintf(msg, args...),
+		Failure: &JUnitFailure{
+			Message: fmt.Sprintf(msg, args...),
+		},
+	})
 	c.FailCount++
 	fmt.Println(Redf(" [fail] "+msg, args...))
 }
 
 // Skipf reports a new skipped test
 func (c *TestResults) Skipf(name, msg string, args ...interface{}) {
+	c.Tests = append(c.Tests, JUnitTestCase{
+		Classname: name,
+		Name:      fmt.Sprintf(msg, args...),
+		SkipMessage: &JUnitSkipMessage{
+			Message: fmt.Sprintf(msg, args...),
+		},
+	})
 	c.SkipCount++
 	fmt.Println(LightCyanf(" [skip] "+msg, args...))
 }
