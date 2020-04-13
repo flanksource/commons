f01c324492f68f37a3c165b86d9241456f1dc636
Moshe Immerman <moshe.immerman@gmail.com>
2020-04-13 15:22:14 +0200

    add more colors

--- console/console.go
+++ console/console.go
@@ -2,12 +2,15 @@ package console
 
 import (
 	"fmt"
+
 	"github.com/flanksource/commons/is"
 )
 
 var (
 	reset        = "\x1b[0m"
 	red          = "\x1b[31m"
+	yellow       = "\x1b[33m"
+	gray         = "\x1b[37m"
 	lightRed     = "\x1b[31;1m"
 	green        = "\x1b[32m"
 	lightGreen   = "\x1b[32;1m"
@@ -19,7 +22,16 @@ var (
 	white        = "\x1b[37;1m"
 	bold         = "\x1b[1m"
 	boldOff      = "\x1b[22m"
+	darkWhite    = "\x1b[38;5;244m"
 )
+
+// DarkWhitef prints and formats msg as dark white
+func DarkF(msg string, args ...interface{}) string {
+	if is.TTY() {
+		return darkWhite + fmt.Sprintf(msg, args...) + reset
+	}
+	return fmt.Sprintf(msg, args...)
+}
 
 // Redf prints and formats msg as red text
 func Redf(msg string, args ...interface{}) string {
@@ -29,10 +41,50 @@ func Redf(msg string, args ...interface{}) string {
 	return fmt.Sprintf(msg, args...)
 }
 
+// LightRedf prints and formats msg as red text
+func LightRedf(msg string, args ...interface{}) string {
+	if is.TTY() {
+		return lightRed + fmt.Sprintf(msg, args...) + reset
+	}
+	return fmt.Sprintf(msg, args...)
+}
+
+// Yellowf prints and formats msg as red text
+func Yellowf(msg string, args ...interface{}) string {
+	if is.TTY() {
+		return yellow + fmt.Sprintf(msg, args...) + reset
+	}
+	return fmt.Sprintf(msg, args...)
+}
+
+// Grayf prints and formats msg as red text
+func Grayf(msg string, args ...interface{}) string {
+	if is.TTY() {
+		return gray + fmt.Sprintf(msg, args...) + reset
+	}
+	return fmt.Sprintf(msg, args...)
+}
+
+// Bluef prints and formats msg as red text
+func Bluef(msg string, args ...interface{}) string {
+	if is.TTY() {
+		return lightBlue + fmt.Sprintf(msg, args...) + reset
+	}
+	return fmt.Sprintf(msg, args...)
+}
+
 // Greenf prints and formats msg as green text
 func Greenf(msg string, args ...interface{}) string {
 	if is.TTY() {
 		return green + fmt.Sprintf(msg, args...) + reset
+	}
+	return fmt.Sprintf(msg, args...)
+}
+
+// Magentaf prints and formats msg as green text
+func Magentaf(msg string, args ...interface{}) string {
+	if is.TTY() {
+		return magenta + fmt.Sprintf(msg, args...) + reset
 	}
 	return fmt.Sprintf(msg, args...)
 }
