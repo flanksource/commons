88d02f125d0d603a78d20bbd3adb350498f86caf
Moshe Immerman <moshe.immerman@gmail.com>
2020-04-13 15:22:38 +0200

    test progress reporters

---
+++ console/progress.go
@@ -0,0 +1,80 @@
+package console
+
+import (
+	"fmt"
+	"io"
+	"strings"
+	"time"
+
+	"github.com/vbauerster/mpb/v5"
+	"github.com/vbauerster/mpb/v5/decor"
+
+	"github.com/flanksource/commons/logger"
+	"github.com/flanksource/commons/timer"
+)
+
+type terminalProgress struct {
+	bar    *mpb.Bar
+	test   *TestResults
+	status string
+}
+type textProgress struct {
+	timer timer.Timer
+	test  *TestResults
+	logger.Logger
+}
+
+func (p *textProgress) Start() {
+	p.Debugf("starting")
+	p.timer = timer.NewTimer()
+
+}
+func (p *textProgress) Done() {
+	p.Infof("finished in %s with %s", p.timer, p.test)
+	p.timer.Stop()
+}
+func (p *textProgress) Status(status string) {
+	p.Infof("%s", status)
+}
+
+func (p *terminalProgress) Start() {
+	p.bar.SetCurrent(1)
+}
+func (p *terminalProgress) Done() {
+	p.bar.SetCurrent(100)
+}
+func (p *terminalProgress) Status(status string) {
+	p.status = status
+}
+
+func NewTextProgress(name string, localTest *TestResults) Progress {
+	return &textProgress{
+		Logger: logger.NewLogger("test", name),
+		test:   localTest,
+	}
+}
+func NewTerminalProgress(name string, localTest *TestResults, progress *mpb.Progress) Progress {
+	tp := &terminalProgress{test: localTest}
+	filler := mpb.NewSpinnerFiller(mpb.DefaultSpinnerStyle, mpb.SpinnerOnLeft)
+	completedFn := mpb.BarFillerFunc(func(w io.Writer, width int, st *decor.Statistics) {
+		if st.Completed {
+			io.WriteString(w, strings.ReplaceAll(tp.test.String(), "\n", ""))
+		} else {
+			io.WriteString(w, tp.status)
+			filler.Fill(w, 27, st)
+		}
+	})
+
+	bar := progress.Add(int64(100), completedFn, mpb.PrependDecorators(
+		decor.NewElapsed(decor.ET_STYLE_MMSS, time.Now()),
+		decor.Name(fmt.Sprintf(" %-20s", name), decor.WC{W: len(name) + 1}),
+	))
+	tp.bar = bar
+	return tp
+}
+
+type Progress interface {
+	Done()
+	Status(status string)
+	Start()
+}
--- console/test_results.go
+++ console/test_results.go
@@ -2,6 +2,7 @@ package console
 
 import (
 	"fmt"
+	"io"
 )
 
 // TestResults contains the status of testing
@@ -10,7 +11,24 @@ type TestResults struct {
 	FailCount int
 	SkipCount int
 	Tests     []JUnitTestCase
-	Name string
+	Name      string
+	Writer    io.Writer
+	Retries   int
+}
+
+func (c *TestResults) Append(other *TestResults) {
+	c.Tests = append(c.Tests, other.Tests...)
+	c.FailCount += other.FailCount
+	c.PassCount += other.PassCount
+	c.SkipCount += other.SkipCount
+}
+
+func (c *TestResults) Retry() {
+	c.PassCount = 0
+	c.SkipCount = 0
+	c.FailCount = 0
+	c.Retries++
+	c.Tests = make([]JUnitTestCase, 0)
 }
 
 func (c TestResults) ToXML() (string, error) {
@@ -27,22 +45,86 @@ func (c TestResults) ToXML() (string, error) {
 	return JUnitTestSuites{
 		Suites: []JUnitTestSuite{
 			JUnitTestSuite{
-				Name: c.Name,
+				Name:      c.Name,
 				TestCases: tests,
 				Tests:     c.PassCount + c.FailCount + c.SkipCount,
 				Failures:  c.FailCount,
 			},
 		},
 	}.ToXML()
-
 }
+
 func (c TestResults) String() string {
-	return fmt.Sprintf("  %d passed, %d skipped, %d failed\n", c.PassCount, c.SkipCount, c.FailCount)
+	s := ""
+	if c.PassCount > 0 {
+		s += Greenf("%d passed", c.PassCount)
+	}
+	if c.SkipCount > 0 {
+		if s != "" {
+			s += " "
+		}
+		s += Magentaf("%d skipped", c.SkipCount)
+	}
+	if c.FailCount > 0 {
+		if s != "" {
+			s += " "
+		}
+		s += Redf("%d failed", c.FailCount)
+	}
+	if c.Retries > 0 {
+		if s != "" {
+			s += " "
+		}
+		s += Redf("with %d retries", c.Retries)
+	}
+	return s
 }
 
 // Done prints the test results to stdout
 func (c *TestResults) Done() {
-	fmt.Println(c.String())
+	c.Println(c.String())
+}
+
+func NewTestResults(name string, writer io.Writer) TestResults {
+	return TestResults{
+		Name:   name,
+		Writer: writer,
+	}
+}
+
+func (c *TestResults) Println(s string) {
+	fmt.Fprintln(c.Writer, s)
+}
+
+var levels = map[string]string{
+	"INFO":  LightCyanf("[INFO]  "),
+	"DEBUG": DarkF("[DEBUG] "),
+	"TRACE": Grayf("[TRACE] "),
+	"ERROR": Redf("[ERROR] "),
+	"FATAL": Redf("[FATAL] "),
+	"WARN":  Yellowf("[WARN]  "),
+}
+
+func (c *TestResults) log(level string, s string, args ...interface{}) {
+	fmt.Fprintf(c.Writer, levels[level]+s+"\n", args...)
+}
+func (c *TestResults) Printf(s string, args ...interface{}) {
+	c.log("INFO", s, args...)
+}
+func (c *TestResults) Infof(s string, args ...interface{}) {
+	c.log("INFO", s, args...)
+}
+func (c *TestResults) Debugf(s string, args ...interface{}) {
+	c.log("DEBUG", s, args...)
+}
+func (c *TestResults) Errorf(s string, args ...interface{}) {
+	c.log("ERROR", s, args...)
+}
+func (c *TestResults) Warnf(s string, args ...interface{}) {
+	c.log("WARN", s, args...)
+}
+func (c *TestResults) Tracef(s string, args ...interface{}) {
+	c.log("TRACE", s, args...)
 }
 
 // Passf reports a new passing test
@@ -52,7 +134,7 @@ func (c *TestResults) Passf(name, msg string, args ...interface{}) {
 		Name:      fmt.Sprintf(msg, args...),
 	})
 	c.PassCount++
-	fmt.Println(Greenf(" [pass] "+msg, args...))
+	c.Println(Greenf(" [pass] "+msg, args...))
 }
 
 // Failf reports a new failing test
@@ -65,7 +147,7 @@ func (c *TestResults) Failf(name, msg string, args ...interface{}) {
 		},
 	})
 	c.FailCount++
-	fmt.Println(Redf(" [fail] "+msg, args...))
+	c.Println(Redf(" [fail] "+msg, args...))
 }
 
 // Skipf reports a new skipped test
@@ -78,7 +160,7 @@ func (c *TestResults) Skipf(name, msg string, args ...interface{}) {
 		},
 	})
 	c.SkipCount++
-	fmt.Println(LightCyanf(" [skip] "+msg, args...))
+	c.Println(LightCyanf(" [skip] "+msg, args...))
 }
 
 func (c *TestResults) SuiteName(name string) *TestResults {
