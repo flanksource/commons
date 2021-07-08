2df1a885ab612bb6d9d3e2c9049d06b17a5e66e6
Tarun Khandelwal <tarun@flanksource.com>
2021-07-08 21:02:19 +0530

    chore: add templateWithDelims

--- text/template_test.go
+++ text/template_test.go
@@ -25,3 +25,22 @@ func TestTemplate(t *testing.T) {
 		})
 	}
 }
+
+func TestTemplateWithDelims(t *testing.T) {
+	vals := map[string]interface{}{
+		"var1": "val1",
+	}
+	g := gomega.NewWithT(t)
+	fixtures := map[string]string{
+		"[[.var1]]":                "val1",
+		`[[ "" | default "foo" ]]`: "foo",
+	}
+
+	for tpl, val := range fixtures {
+		t.Run(tpl, func(t *testing.T) {
+			result, err := TemplateWithDelims(tpl, "[[", "]]", vals)
+			g.Expect(err).To(BeNil())
+			g.Expect(result).To(Equal(val))
+		})
+	}
+}
