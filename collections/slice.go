fc7274bf51bb65728fc4adef92e137dacf7da83f
Aditya Thebe <contact@adityathebe.com>
2025-01-28 12:29:11 +0545

    feat: support exclude all & patterns on exclusions as well (#260)
    * fix: MatchItems
    
    * commutative: The order should't matter
    * handle exclusion only patterns
    
    * feat: support exclude all & matterns on exclusions as well
    
    * more test cases

--- collections/slice.go
+++ collections/slice.go
@@ -2,6 +2,7 @@ package collections
 
 import (
 	"net/url"
+	"slices"
 	"strings"
 )
 
@@ -73,46 +74,102 @@ func Append[T any](slices ...[]T) []T {
 	return output
 }
 
-// matchItems returns true if any of the items in the list match the item.
-// negative matches are supported by prefixing the item with a "!".
+// matchItems returns true if any of the patterns in the list match the item.
+// negative matches are supported by prefixing the item with a "!" and
+// takes precendence over positive match.
 // * matches everything
 // to match prefix and suffix use "*" accordingly.
-func MatchItems(item string, items ...string) bool {
-	if len(items) == 0 {
+func MatchItems(item string, patterns ...string) bool {
+	if len(patterns) == 0 {
 		return true
 	}
 
-	for _, i := range items {
-		i = strings.TrimSpace(i)
+	slices.SortFunc(patterns, sortPatterns)
 
-		i, _ := url.QueryUnescape(i)
+	for _, p := range patterns {
+		pattern, err := url.QueryUnescape(strings.TrimSpace(p))
+		if err != nil {
+			continue
+		}
 
-		if strings.HasPrefix(i, "!") {
-			if item == strings.TrimPrefix(i, "!") {
+		if strings.HasPrefix(pattern, "!") {
+			if matchPattern(item, strings.TrimPrefix(pattern, "!")) {
 				return false
 			}
 
 			continue
 		}
 
-		if i == "*" || item == i {
+		if matchPattern(item, pattern) {
 			return true
 		}
+	}
 
-		if strings.HasPrefix(i, "*") {
-			if strings.HasSuffix(item, strings.TrimPrefix(i, "*")) {
-				return true
-			}
+	//nolint:gosimple
+	//lint:ignore S1008 ...
+	if allExclusions(patterns) {
+		// If all the filters were exlusions, and none of the exclusions excluded the item, then it's a match
+		return true
+	}
+
+	return false
+}
+
+func matchPattern(item, pattern string) bool {
+	if pattern == "*" || item == pattern {
+		return true
+	}
+
+	if strings.HasPrefix(pattern, "*") {
+		if strings.HasSuffix(item, strings.TrimPrefix(pattern, "*")) {
+			return true
 		}
+	}
 
-		if strings.HasSuffix(i, "*") {
-			if strings.HasPrefix(item, strings.TrimSuffix(i, "*")) {
-				return true
-			}
+	if strings.HasSuffix(pattern, "*") {
+		if strings.HasPrefix(item, strings.TrimSuffix(pattern, "*")) {
+			return true
 		}
 	}
 
 	return false
+}
+
+// sortPatterns defines the priority for sorting:
+// exclusions ("!") have higher priority than other patterns.
+func sortPatterns(a, b string) int {
+	if a == "!*" {
+		return -1
+	} else if b == "!*" {
+		return 1
+	}
+
+	if strings.HasPrefix(a, "!") {
+		return -1
+	} else if strings.HasPrefix(b, "!") {
+		return 1
+	}
+
+	return 0
+}
+
+func allExclusions(patterns []string) bool {
+	if len(patterns) == 0 {
+		return false
+	}
+
+	for _, pattern := range patterns {
+		pattern = strings.TrimSpace(pattern)
+		if pattern == "" {
+			return false
+		}
+
+		if !strings.HasPrefix(pattern, "!") {
+			return false
+		}
+	}
+
+	return true
 }
 
 func DeleteEmptyStrings(s []string) []string {
